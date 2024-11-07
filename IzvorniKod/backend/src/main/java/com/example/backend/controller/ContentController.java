d70f609ac1ddc0d409e86632df6d9ca4e8bcc160
Viktor Bogojević <viktor.bogojevic@fer.hr>
2024-11-07 19:11:31 +0100

    spremanje

---
+++ IzvorniKod/backend/src/main/java/com/example/backend/controller/ContentController.java
@@ -0,0 +1,90 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.LoginDto;
+import com.example.backend.security.JwtService;
+import com.example.backend.security.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+public class ContentController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "home page";
+    }
+
+    @GetMapping("/volunteer/home")
+    public String volunteer_home() {
+        return "VOLUNTEER home page";
+    }
+
+    @GetMapping("/organization/home")
+    public String organization_home() {
+        return "ORGANIZATION home page";
+    }
+
+//    @PostMapping("/authenticate")
+//    public String authenticateAndGetToken(@RequestBody LoginDto loginDto) {
+//        // provjerava jesu li username i password dobri
+//        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+//                loginDto.getUsername(),  loginDto.getPassword()
+//        ));
+//
+//        // ako je dobro, vrati token
+//        if (authentication.isAuthenticated()){
+//            return jwtService.generateToken(myUserDetailsService.loadUserByUsername(loginDto.getUsername()));
+//        }else{
+//            throw new UsernameNotFoundException("invalid credentials :(");
+//        }
+//    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticateAndGetToken(@RequestBody LoginDto loginDto) {
+        try {
+            // Provodi autentifikaciju
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword())
+            );
+
+            // Ako je autentifikacija uspješna, generira JWT token
+            if (authentication.isAuthenticated()) {
+                String token = jwtService.generateToken(myUserDetailsService.loadUserByUsername(loginDto.getUsername()));
+
+                // Vraća JWT token u odgovoru
+                Map<String, String> response = new HashMap<>();
+                response.put("jwt", token);
+                return ResponseEntity.ok(response);
+            } else {
+                // Ovo neće biti izvršeno jer je autentifikacija provjerena prije
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed");
+            }
+
+        } catch (Exception e) {
+            // Ako je autentifikacija neuspješna, vraća odgovarajuću poruku o grešci
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("error", "Invalid username or password");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+        }
+    }
+
+}
---
+++ IzvorniKod/backend/src/main/java/com/example/backend/model/MyUser.java
@@ -0,0 +1,73 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class MyUser {
+
+    @SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    @Id
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // VOLUNTEER, ORGANIZATION, ADMIN
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "MyUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
