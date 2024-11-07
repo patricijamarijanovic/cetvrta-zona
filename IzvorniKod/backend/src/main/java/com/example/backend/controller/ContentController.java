f43c444cf9881efd9597d009346c54926793312f
pata <patricija.marijanovic955@gmail.com>
2024-11-07 15:18:06 +0100

    Connect to frontend

--- IzvorniKod/backend/src/main/java/com/example/backend/controller/ContentController.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/controller/ContentController.java
@@ -10,15 +10,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class ContentController {
 
     @Autowired
--- IzvorniKod/backend/src/main/java/com/example/backend/controller/RegistrationController.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/controller/RegistrationController.java
@@ -14,12 +14,10 @@ import com.example.backend.service.VolunteerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/register")
 public class RegistrationController {
     @Autowired
