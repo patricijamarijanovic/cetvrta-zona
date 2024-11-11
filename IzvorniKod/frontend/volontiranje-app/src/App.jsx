db789fad40b5e42977fcf18768c368db487346e0
sten <vito.stanic@fer.hr>
2024-11-12 00:18:49 +0100

    App.jsx updated for running HomePage
    ažurirana glavna datoteka aplikacije za pokretanje homePage-a i izbrisani .css importi u loginu te loginu za volontera i organizaciju

--- IzvorniKod/frontend/volontiranje-app/src/App.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/App.jsx
@@ -1,34 +1,30 @@
-import VolunteerLogin from "./components/VolunteerLogin"
-import OrganizationLogin from "./components/OrganizationLogin"
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import Options from "./components/Options"
-import Login from "./components/Login"
-import VolunteerHomePage from "../pages/VolunteerHomePage"
-import OrganizationHomePage from "../pages/OrganizationHomePage"
-import HomePage from "../pages/HomePage"
-import NotAuthorized from "../pages/NotAuthorized"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./components/Login";
+import Options from "./components/Options";
+import OrganizationLogin from "./components/OrganizationLogin";
+import VolunteerLogin from "./components/VolunteerLogin";
+
+import HomePage from "./pages/HomePage";
+import NotAuthorized from "./pages/NotAuthorized";
+import OrganizationHomePage from "./pages/OrganizationHomePage";
+import VolunteerHomePage from "./pages/VolunteerHomePage";
 
 function App() {
-  
-
   return (
-
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<HomePage />}/>
-    <Route path="/register" element={<Options />} /> 
-    <Route path='/register/volunteer' element={<VolunteerLogin />}/>
-    <Route path='/register/organization' element={<OrganizationLogin />}/>
-    <Route path='/login' element={<Login />}/>
-    <Route path='/volunteer/home' element={<VolunteerHomePage />}/>
-    <Route path='/organization/home' element={<OrganizationHomePage />}/>
-    <Route path='/not-authorized' element={<NotAuthorized />}/>
-
-   </Routes>
-   </BrowserRouter>
-
-   
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Options />} />
+        <Route path="/register/volunteer" element={<VolunteerLogin />} />
+        <Route path="/register/organization" element={<OrganizationLogin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/volunteer/home' element={<VolunteerHomePage />}/>
+        <Route path='/organization/home' element={<OrganizationHomePage />}/>
+        <Route path='/not-authorized' element={<NotAuthorized />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- IzvorniKod/frontend/volontiranje-app/src/components/Login.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
-import './styles/login.css';
 import axios from 'axios';
 
 
