f8bb0b453121ab0bf11a95a499c42122c555f4a7
sten <vito.stanic@fer.hr>
2024-11-13 15:06:00 +0100

    Update NotAuthorized.jsx
    Dodan dizajn na stranicu NotAuthorized

--- IzvorniKod/frontend/volontiranje-app/src/pages/NotAuthorized.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/NotAuthorized.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotAuthorized() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="not-authorized">
-            <h2>Stranica nije dostupna</h2>
-            <p>Nemate potrebna dopuštenja za pristup ovoj stranici.</p>
-            <button onClick={() => navigate(-1)}>Povratak</button>
-        </div>
-    );
+  return (
+    <div className="p-8 bg-slate-600 rounded-b-3xl text-white">
+      <h2 className="font-bold text-5xl">Pristup odbijen</h2>
+      <p className="text-xl py-2">
+        Nemate potrebna dopuštenja za pristup ovoj stranici.
+      </p>
+      <button
+        className="bg-yellow-300 text-black py-2 px-4 rounded-s-3xl rounded-e-3xl hover:bg-yellow-400"
+        onClick={() => navigate(-1)}
+      >
+        Povratak
+      </button>
+    </div>
+  );
 }
 
 export default NotAuthorized;
