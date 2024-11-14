4ba805995247f0bf34ef57980f5041550024e985
jmalicc <jakov.z.malic@gmail.com>
2024-11-14 10:32:06 +0100

    unsqueezing the organization projects
    yohoho

--- IzvorniKod/frontend/volontiranje-app/src/pages/OrganizationHomePage.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/OrganizationHomePage.jsx
@@ -76,7 +76,7 @@ function OrganizationHomePage() {
                 </button>
               </div>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div>
                 <ActivitiesListOrg />
               </div>
             )}
