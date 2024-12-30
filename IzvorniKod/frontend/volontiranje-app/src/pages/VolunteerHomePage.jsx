000953bea4fd724f7c9e8c2466e52ff22d4adf42
sten <vito.stanic@fer.hr>
2024-12-30 19:53:35 +0100

    update NavBarVol.jsx
    dodan izbornik kada se stisne na logo profila

--- IzvorniKod/frontend/volontiranje-app/src/pages/VolunteerHomePage.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/VolunteerHomePage.jsx
@@ -11,61 +11,57 @@ import ActivitiesList from "./assets/activitiesList";
 const BACK_URL = "http://localhost:8080";
 
 function VolunteerHomePage() {
-  console.log("u volonteskom home pageu1")
-    const [message, setMessage] = useState('');
-    const [error, setError] = useState('');
-    
-    const navigate = useNavigate();
+  console.log("u volonteskom home pageu1");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
+  const navigate = useNavigate();
 
-    function signOut() {
-        localStorage.clear();
-        navigate('/');
+  function signOut() {
+    localStorage.clear();
+    navigate("/");
+  }
+  useEffect(() => {
+    console.log("u volonteskom home pageu");
+    const token = localStorage.getItem("token");
+    console.log("Tu je token!" + token);
+    if (!token) {
+      console.log("nema tokena!");
+      navigate("/not-authorized", { replace: true });
 
+      return;
     }
-    useEffect(() => {
-        console.log("u volonteskom home pageu")
-        const token = localStorage.getItem('token');
-        console.log('Tu je token!' + token)
-        if (!token) {
 
-          console.log('nema tokena!')
-          navigate('/not-authorized', { replace: true });
-
-          return;
+    console.log("saljem token na backend");
+    axios
+      .get(`${BACK_URL}/volunteer/home`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setMessage(response.data);
+        console.log("Tu sam!");
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+        console.error("Greška prilikom provjere tokena:", error.message); // Dodano ispisivanje glavne poruke
+        if (error.response) {
+          console.error("Odgovor poslužitelja:", error.response.data); // Dodano ispisivanje server response data
         }
-    
-   
-        console.log("saljem token na backend")
-        axios.get(`${BACK_URL}/volunteer/home`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          setMessage(response.data);
-          console.log('Tu sam!')
-        })
-        .catch((error) => {
-          console.log("error: ", error)
-          console.error('Greška prilikom provjere tokena:', error.message); // Dodano ispisivanje glavne poruke
-          if (error.response) {
-            console.error('Odgovor poslužitelja:', error.response.data); // Dodano ispisivanje server response data
-          }
-          navigate('/not-authorized', { replace: true });
-        });
-      }, [navigate]); 
-  
+        navigate("/not-authorized", { replace: true });
+      });
+  }, [navigate]);
 
-    return(
-      <div className="relative min-h-screen">
-        <div className="bg-slate-600 rounded-b-3xl text-white">
-          <NavBarLoggedIn />
-          <Motivation />
-        </div>
-          <ActivitiesList />
+  return (
+    <div className="relative min-h-screen">
+      <div className="bg-slate-600 rounded-b-3xl text-white">
+        <NavBarLoggedIn />
+        <Motivation />
       </div>
-    )
+      <ActivitiesList />
+    </div>
+  );
 }
 
 export default VolunteerHomePage;
