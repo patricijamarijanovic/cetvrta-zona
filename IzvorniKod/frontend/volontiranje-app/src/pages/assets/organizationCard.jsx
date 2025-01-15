e4cf9f6c9919c15224c8c951d1f656aa4904e3d6
jmalicc <jakov.malic@fer.unizg.hr>
2025-01-15 14:35:55 +0100

    Update organizationCard.jsx

--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/organizationCard.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/organizationCard.jsx
@@ -1,16 +1,13 @@
-
 function OrganizationCard({OrganizationName, image}) {
     return (
-      <a href="#">
-        <div className="bg-gray-100 rounded-lg shadow-md p-6 flex flex-col">
-          <img
-            src={image}
-            alt={`${OrganizationName}`}
-            className="rounded-lg object-cover h-40 w-full mb-4"
-          />
-          <h2 className="text-lg font-semibold text-gray-800">{OrganizationName}</h2>
-        </div>
-      </a>
+      <div className="bg-gray-100 rounded-lg shadow-md p-6 flex flex-col">
+        <img
+          src={image}
+          alt={`${OrganizationName}`}
+          className="rounded-lg object-cover h-40 w-full mb-4"
+        />
+        <h2 className="text-lg font-semibold text-gray-800">{OrganizationName}</h2>
+      </div>
     );
   }
   
