31124ebae86d84c3185e9696452e678b0510ce36
pata <patricija.marijanovic955@gmail.com>
2025-01-16 23:18:25 +0100

    Revert "mijenjanje kartice aktivnosti i svih njenih referenci"
    This reverts commit 374211f752f2cfcd3534f9fe85f47d95a2895124.

--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/activitiesListOrg.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/activitiesListOrg.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Card from "./card";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 // const BACK_URL = "backend-qns7.onrender.com";
 // const BACK_URL = "https://backend-qns7.onrender.com";
@@ -24,39 +24,35 @@ function ActivitiesList() {
       return;
     }
 
-    axios
-      .get(`${BACK_URL}/organization/home`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
+    axios.get(`${BACK_URL}/organization/home`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data)
 
         const ids = response.data.map((org) => org.projectID);
-        console.log("ids: " + ids);
+        console.log("ids: " + ids)
 
         // Pokreni zahtjeve za sve ID-eve paralelno
         Promise.all(
-          ids.map(
-            (projectId) =>
-              axios
-                .get(`${BACK_URL}/home/project-picture/${projectId}`, {
-                  responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
-                })
-                .then((res) => {
-                  if (res.status === 204) {
-                    return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
-                  } else {
-                    const imageBlob = new Blob([res.data], {
-                      type: "image/jpeg",
-                    });
-                    const imageUrl = URL.createObjectURL(imageBlob);
-                    console.log("imam sliku!! " + imageUrl);
-                    return imageUrl; // Vrati URL slike
-                  }
-                })
-                .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
+          ids.map((projectId) =>
+            axios
+              .get(`${BACK_URL}/home/project-picture/${projectId}`, {
+                responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
+              })
+              .then((res) => {
+                if (res.status === 204) {
+                  return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
+                } else {
+                  const imageBlob = new Blob([res.data], { type: "image/jpeg" });
+                  const imageUrl = URL.createObjectURL(imageBlob);
+                  console.log("imam sliku!! " + imageUrl);
+                  return imageUrl; // Vrati URL slike
+                }
+              })
+              .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
           )
         )
           .then((logosArray) => {
@@ -76,31 +72,28 @@ function ActivitiesList() {
       });
   }, []);
 
-  if (loading)
-    return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
+  if (loading) return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
   if (error) return <p className="p-8 text-red-500">{error}</p>;
-
+  
   return (
     <section className="p-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {activities.map((activity, index) => (
-          <Link to={`/organization/activity/${activity.projectID}`}>
-            <Card
-              key={index}
-              title={activity.projectname}
-              location={activity.projectlocation}
-              startDate={activity.beginningdate}
-              endDate={activity.enddate}
-              organization={activity.organizationName}
-              image={pics[index]}
-              urgency={activity.urgent}
-              category={activity.typeofwork}
-            />
+           <Link to={`/organization/activity/${activity.projectID}`}>
+          <Card
+            key={index}
+            title={activity.projectname}
+            location={activity.projectlocation}
+            dates={`From: ${activity.beginningdate} To: ${activity.enddate}`}
+            organization={activity.organizationName}
+            image={pics[index]}
+          />
           </Link>
         ))}
       </div>
     </section>
   );
+  
 }
 
 export default ActivitiesList;
--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/futureActivitiesList.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/futureActivitiesList.jsx
@@ -3,6 +3,7 @@ import Card from "./card";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+
 // const BACK_URL = "backend-qns7.onrender.com";
 // const BACK_URL = "https://backend-qns7.onrender.com";
 const BACK_URL = "http://localhost:8080";
@@ -17,38 +18,37 @@ function FutureActivitiesList() {
   console.log(role);
 
   useEffect(() => {
-    axios
-      .get(`${BACK_URL}/volunteer/future-activities`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
+    axios.get(`${BACK_URL}/volunteer/future-activities`,
+        {
+          headers:
+          {
+            Authorization: `Bearer ${token}` 
+          },
+        }
+    )
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data)
 
         const ids = response.data.map((org) => org.projectID);
-        console.log("ids: " + ids);
+        console.log("ids: " + ids)
         // Pokreni zahtjeve za sve ID-eve paralelno
         Promise.all(
-          ids.map(
-            (projectId) =>
-              axios
-                .get(`${BACK_URL}/home/project-picture/${projectId}`, {
-                  responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
-                })
-                .then((res) => {
-                  if (res.status === 204) {
-                    return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
-                  } else {
-                    const imageBlob = new Blob([res.data], {
-                      type: "image/jpeg",
-                    });
-                    const imageUrl = URL.createObjectURL(imageBlob);
-                    console.log("imam sliku!! " + imageUrl);
-                    return imageUrl; // Vrati URL slike
-                  }
-                })
-                .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
+          ids.map((projectId) =>
+            axios
+              .get(`${BACK_URL}/home/project-picture/${projectId}`, {
+                responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
+              })
+              .then((res) => {
+                if (res.status === 204) {
+                  return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
+                } else {
+                  const imageBlob = new Blob([res.data], { type: "image/jpeg" });
+                  const imageUrl = URL.createObjectURL(imageBlob);
+                  console.log("imam sliku!! " + imageUrl);
+                  return imageUrl; // Vrati URL slike
+                }
+              })
+              .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
           )
         )
           .then((logosArray) => {
@@ -70,40 +70,34 @@ function FutureActivitiesList() {
 
   const getLink = (activityId) => {
     if (role === "ROLE_ORGANIZATION") {
-      return `/organization/activity/${activityId}`;
+        return `/organization/activity/${activityId}`;
     } else if (role === "ROLE_VOLUNTEER") {
-      return `/volunteer/activity/${activityId}`;
+        return `/volunteer/activity/${activityId}`;
     } else {
-      return `/activity/${activityId}`; // Defaultni link ako nema role
+        return `/activity/${activityId}`; // Defaultni link ako nema role
     }
-  };
+};
 
-  if (loading)
-    return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
+  if (loading) return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
   if (error) return <p className="p-8 text-red-500">{error}</p>;
 
   return (
     <section className="p-8">
-      <h1 className="text-3xl font-bold text-gray-800 mb-8">
-        Buduće aktivnosti{" "}
-      </h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-8">Buduće aktivnosti </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {activities.length === 0 ? (
           <h1>Nažalost trenutačno nema aktivnosti u ovoj kategoriji :'( </h1>
         ) : (
           activities.map((activity, index) => (
             <Link to={getLink(activity.projectID)}>
-              <Card
-                key={index}
-                title={activity.projectname}
-                location={activity.projectlocation}
-                startDate={activity.beginningdate}
-                endDate={activity.enddate}
-                organization={activity.organizationName}
-                image={"/images/nekaovog.jpg"}
-                urgency={activity.urgent}
-                category={activity.typeofwork}
-              />
+            <Card
+              key={index}
+              title={activity.projectname}
+              location={activity.projectlocation}
+              dates={`From: ${activity.beginningdate} To: ${activity.enddate}`}
+              organization={activity.organizationName}
+              image={pics[index]}
+            />
             </Link>
           ))
         )}
