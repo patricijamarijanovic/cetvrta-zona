d207e71a09ea40eb2b96ea2782ed38b7783720ee
pata <patricija.marijanovic955@gmail.com>
2025-01-16 17:59:42 +0100

    dodavanje default korisnika s proslim projektom

--- IzvorniKod/backend/src/main/java/com/example/backend/BackendApplication.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,15 @@
 package com.example.backend;
 
-import com.example.backend.model.MyUser;
-import com.example.backend.model.Role;
-import com.example.backend.repository.MyUserRepository;
+import com.example.backend.model.*;
+import com.example.backend.repository.*;
 
+import com.example.backend.service.VolunteerService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -17,14 +19,53 @@ public class BackendApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(MyUserRepository userRepository) {
+	CommandLineRunner commandLineRunner(MyUserRepository userRepository, OrganizationRepository organizationRepository, VolunteerService volunteerService, VolunteerRepository volunteerRepository, ProjectRepository projectRepository, ApplicationRepository applicationRepository) {
 		return args -> {
 			MyUser user = new MyUser();
 			user.setRole(Role.ADMIN);
 			user.setUsername("admin");
 			user.setEmail("admin@example.com");
 			user.setPassword("$2a$10$hd.K4YAUxErbA/F1IQvsAetXSaRHBG80cKTKFaJhUBuwGDhZDwu7a");
+			user.setVerified(true);
 			userRepository.save(user);
+
+			Volunteer user2 = new Volunteer();
+			user2.setRole(Role.VOLUNTEER);
+			user2.setUsername("volonter");
+			user2.setEmail("mail@gmail.com");
+			user2.setPassword("$2a$10$hd.K4YAUxErbA/F1IQvsAetXSaRHBG80cKTKFaJhUBuwGDhZDwu7a");
+			user2.setVerified(true);
+			volunteerRepository.save(user2);
+
+			Organization user3 = new Organization();
+			user3.setRole(Role.ORGANIZATION);
+			user3.setUsername("klaiceva");
+			user3.setEmail("mail2@gmail.com");
+			user3.setPassword("$2a$10$hd.K4YAUxErbA/F1IQvsAetXSaRHBG80cKTKFaJhUBuwGDhZDwu7a");
+			user3.setVerified(true);
+			user3.setOrganizationName("Klaiceva bolnica");
+			user3.setDescription("ovo je opis organizacije");
+			organizationRepository.save(user3);
+
+			Project project = new Project();
+			project.setUrgent(true);
+			project.setProjectName("Akcija doniranja krvi");
+			project.setProjectDesc("opis");
+			project.setOrganizationID(user3.getId());
+			project.setLocation("Zagreb");
+			project.setEndDate(LocalDate.of(2024, 1, 31));
+			project.setStartDate(LocalDate.of(2024, 2, 28));
+			project.setMaxNumVolunteers(30);
+			project.setNumVolunteers(0);
+			project.setTypeOfWork(TypeOfWork.DJECA);
+			projectRepository.save(project);
+			
+			Application application = new Application();
+			application.setVolunteerId(user2.getId());
+			application.setProjectId(project.getProjectId());
+			application.setStatus(ApplicationStatus.ACCEPTED);
+			applicationRepository.save(application);
+
 		};
 	}
 	
