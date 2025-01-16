b39c3d7dfe1389e906ff4c127dc404dd204ff5f7
ViktorBogojevic <viktor.bogojevic@fer.hr>
2025-01-16 17:52:38 +0100

    dodana mogucnost pregleda svih recenzija na neki projekt

---
+++ IzvorniKod/backend/src/main/java/com/example/backend/controller/ReviewAndResponseController.java
@@ -0,0 +1,25 @@
+package com.example.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.dto.ReviewDisplayDto;
+import com.example.backend.service.ReviewAndResponseService;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:5173", "https://volontirajsnama.onrender.com"})
+public class ReviewAndResponseController {
+	
+	@Autowired
+	private ReviewAndResponseService reviewAndResponseService;
+	
+	@GetMapping("/getreviews/{projectID}")
+	public List<ReviewDisplayDto> get_all_reviews_by_projectID(@PathVariable Integer projectId) {
+		return reviewAndResponseService.get_all_reviews_by_projectID(projectId);
+	}
+}
