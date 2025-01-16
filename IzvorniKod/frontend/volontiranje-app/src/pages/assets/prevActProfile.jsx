718f2f66c172d4f8573b1179b6a7574c77bc9ce7
ivanadebeljak <id54770@fer.hr>
2025-01-16 23:21:16 +0100

    Revert "mijenjanje kartice aktivnosti i svih njenih referenci"
    This reverts commit 374211f752f2cfcd3534f9fe85f47d95a2895124.

--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/prevActProfile.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/prevActProfile.jsx
@@ -3,11 +3,12 @@ import Card from "./card";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+
 // const BACK_URL = "backend-qns7.onrender.com";
 // const BACK_URL = "https://backend-qns7.onrender.com";
 const BACK_URL = "http://localhost:8080";
 
-function PrevActProfile({ volunteerID }) {
+function PrevActProfile({volunteerID}) {
   const token = localStorage.getItem("token");
   const [activities, setActivities] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -17,38 +18,37 @@ function PrevActProfile({ volunteerID }) {
   console.log(role);
 
   useEffect(() => {
-    axios
-      .get(`${BACK_URL}/home/volunteer/previous-activities/${volunteerID}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
+    axios.get(`${BACK_URL}/home/volunteer/previous-activities/${volunteerID}`,
+      {        
+        headers:
+        {            
+          Authorization: `Bearer ${token}` 
         },
-      })
+      }
+    )
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data)
 
         const ids = response.data.map((org) => org.projectID);
-        console.log("ids: " + ids);
+        console.log("ids: " + ids)
         // Pokreni zahtjeve za sve ID-eve paralelno
         Promise.all(
-          ids.map(
-            (projectId) =>
-              axios
-                .get(`${BACK_URL}/home/project-picture/${projectId}`, {
-                  responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
-                })
-                .then((res) => {
-                  if (res.status === 204) {
-                    return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
-                  } else {
-                    const imageBlob = new Blob([res.data], {
-                      type: "image/jpeg",
-                    });
-                    const imageUrl = URL.createObjectURL(imageBlob);
-                    console.log("imam sliku!! " + imageUrl);
-                    return imageUrl; // Vrati URL slike
-                  }
-                })
-                .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
+          ids.map((projectId) =>
+            axios
+              .get(`${BACK_URL}/home/project-picture/${projectId}`, {
+                responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
+              })
+              .then((res) => {
+                if (res.status === 204) {
+                  return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
+                } else {
+                  const imageBlob = new Blob([res.data], { type: "image/jpeg" });
+                  const imageUrl = URL.createObjectURL(imageBlob);
+                  console.log("imam sliku!! " + imageUrl);
+                  return imageUrl; // Vrati URL slike
+                }
+              })
+              .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
           )
         )
           .then((logosArray) => {
@@ -68,44 +68,36 @@ function PrevActProfile({ volunteerID }) {
       });
   }, []);
 
+
   const getLink = (activityId) => {
     if (role === "ROLE_ORGANIZATION") {
-      return `/organization/activity/${activityId}`;
+        return `/organization/activity/${activityId}`;
     } else if (role === "ROLE_VOLUNTEER") {
-      return `/volunteer/activity/${activityId}`;
+        return `/volunteer/activity/${activityId}`;
     } else {
-      return `/activity/${activityId}`; // Defaultni link ako nema role
+        return `/activity/${activityId}`; // Defaultni link ako nema role
     }
-  };
+};
 
-  if (loading)
-    return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
+  if (loading) return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
   if (error) return <p className="p-8 text-red-500">{error}</p>;
 
   return (
     <section className="flex flex-col mb-4">
-      <h4 className="text-gray-600 font-medium mb-2">
-        Prethodne aktivnosti volontera
-      </h4>
+      <h4 className="text-gray-600 font-medium mb-2">Prethodne aktivnosti volontera</h4>
       <div>
         {activities.length === 0 ? (
-          <p className="text-gray-800">
-            Volonter još nije sudjelovao u niti jednoj aktivnosti.
-          </p>
+          <p className="text-gray-800">Volonter još nije sudjelovao u niti jednoj aktivnosti.</p>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {activities.map((activity, index) => (
               <Link to={getLink(activity.projectID)} key={index}>
                 <Card
-                  key={index}
                   title={activity.projectname}
                   location={activity.projectlocation}
-                  startDate={activity.beginningdate}
-                  endDate={activity.enddate}
+                  dates={`From: ${activity.beginningdate} To: ${activity.enddate}`}
                   organization={activity.organizationName}
                   image={pics[index]}
-                  urgency={activity.urgent}
-                  category={activity.typeofwork}
                 />
               </Link>
             ))}
@@ -115,5 +107,6 @@ function PrevActProfile({ volunteerID }) {
     </section>
   );
 }
+
 
 export default PrevActProfile;
