b189198568192ce8be55d2e2a9136211e7d5a0d1
ivanadebeljak <id54770@fer.hr>
2025-01-17 00:09:16 +0100

    Revert "mijenjanje kartice aktivnosti i svih njenih referenci"
    This reverts commit 374211f752f2cfcd3534f9fe85f47d95a2895124.

--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/activitiesList.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/activitiesList.jsx
@@ -3,6 +3,7 @@ import Card from "./card";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+
 // const BACK_URL = "backend-qns7.onrender.com";
 // const BACK_URL = "https://backend-qns7.onrender.com";
 const BACK_URL = "http://localhost:8080";
@@ -17,35 +18,31 @@ function ActivitiesList() {
   const [pics, setPics] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`${BACK_URL}/home/recent-activities`)
+    axios.get(`${BACK_URL}/home/recent-activities`)
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data)
 
         const ids = response.data.map((org) => org.projectID);
-        console.log("ids: " + ids);
+        console.log("ids: " + ids)
 
         // Pokreni zahtjeve za sve ID-eve paralelno
         Promise.all(
-          ids.map(
-            (projectId) =>
-              axios
-                .get(`${BACK_URL}/home/project-picture/${projectId}`, {
-                  responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
-                })
-                .then((res) => {
-                  if (res.status === 204) {
-                    return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
-                  } else {
-                    const imageBlob = new Blob([res.data], {
-                      type: "image/jpeg",
-                    });
-                    const imageUrl = URL.createObjectURL(imageBlob);
-                    console.log("imam sliku!! " + imageUrl);
-                    return imageUrl; // Vrati URL slike
-                  }
-                })
-                .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
+          ids.map((projectId) =>
+            axios
+              .get(`${BACK_URL}/home/project-picture/${projectId}`, {
+                responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
+              })
+              .then((res) => {
+                if (res.status === 204) {
+                  return "/images/nekaovog.jpg"; // Ako nema slike, postavi default
+                } else {
+                  const imageBlob = new Blob([res.data], { type: "image/jpeg" });
+                  const imageUrl = URL.createObjectURL(imageBlob);
+                  console.log("imam sliku!! " + imageUrl);
+                  return imageUrl; // Vrati URL slike
+                }
+              })
+              .catch(() => "/images/nekaovog.jpg") // U slučaju greške, postavi default
           )
         )
           .then((logosArray) => {
@@ -67,16 +64,15 @@ function ActivitiesList() {
 
   const getLink = (activityId) => {
     if (role === "ROLE_ORGANIZATION") {
-      return `/organization/activity/${activityId}`;
+        return `/organization/activity/${activityId}`;
     } else if (role === "ROLE_VOLUNTEER") {
-      return `/volunteer/activity/${activityId}`;
+        return `/volunteer/activity/${activityId}`;
     } else {
-      return `/activity/${activityId}`; // Defaultni link ako nema role
+        return `/activity/${activityId}`; // Defaultni link ako nema role
     }
-  };
+};
 
-  if (loading)
-    return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
+  if (loading) return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
   if (error) return <p className="p-8 text-red-500">{error}</p>;
 
   return (
@@ -88,17 +84,14 @@ function ActivitiesList() {
         ) : (
           activities.map((activity, index) => (
             <Link to={getLink(activity.projectID)}>
-              <Card
-                key={index}
-                title={activity.projectname}
-                location={activity.projectlocation}
-                startDate={activity.beginningdate}
-                endDate={activity.enddate}
-                organization={activity.organizationName}
-                image={pics[index]}
-                urgency={activity.urgent}
-                category={activity.typeofwork}
-              />
+            <Card
+              key={index}
+              title={activity.projectname}
+              location={activity.projectlocation}
+              dates={`From: ${activity.beginningdate} To: ${activity.enddate}`}
+              organization={activity.organizationName}
+              image={pics[index]}
+            />
             </Link>
           ))
         )}
