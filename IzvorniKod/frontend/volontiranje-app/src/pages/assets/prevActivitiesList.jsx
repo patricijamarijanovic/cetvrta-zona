32b792662d950cc94a914a4f24e6257d89f2132a
sten <vito.stanic@fer.hr>
2025-01-17 13:27:11 +0100

    Popravljeni fiksovi sa karticama aktivnosti
    također potrebna je prepravka u 4 datoteke na stranici mojih aktivnosti. To su inProgressActivitiesList.jsx, futureActivitiesList.jsx, pendingActivitiesList.jsx i prevActivitiesList.jsx

--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/prevActivitiesList.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/prevActivitiesList.jsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import Card from "./card";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { FacebookShareButton, FacebookIcon } from 'react-share';
-
+import { FacebookShareButton, FacebookIcon } from "react-share";
 
 // const BACK_URL = "backend-qns7.onrender.com";
 // const BACK_URL = "https://backend-qns7.onrender.com";
@@ -18,46 +17,45 @@ function PrevActivitiesList() {
   const [pics, setPics] = useState([]);
 
   const shareUrl = "https://volontirajsnama.onrender.com/"; // URL koji dijelim
-  const shareMessage = "Sudjelovao/la sam u volonterskoj aktivnosti! Pridruži se i ti!";
+  const shareMessage =
+    "Sudjelovao/la sam u volonterskoj aktivnosti! Pridruži se i ti!";
 
   console.log(role);
 
   useEffect(() => {
-    axios.get(`${BACK_URL}/volunteer/previous-activities`,
-      {
-        headers:
-        {
-          Authorization: `Bearer ${token}` 
+    axios
+      .get(`${BACK_URL}/volunteer/previous-activities`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-      }
-    )
+      })
       .then((response) => {
-        console.log(response.data)
+        console.log(response.data);
 
         const ids = response.data.map((org) => org.projectID);
-        console.log("ids: " + ids)
+        console.log("ids: " + ids);
         // Pokreni zahtjeve za sve ID-eve paralelno
         Promise.all(
-          ids.map((projectId) =>
-            axios
-              .get(`${BACK_URL}/home/project-picture/${projectId}`, {
-                responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
-              })
-              .then((res) => {
-                if (res.status === 204) {
+          ids.map(
+            (projectId) =>
+              axios
+                .get(`${BACK_URL}/home/project-picture/${projectId}`, {
+                  responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
+                })
+                .then((res) => {
+                  if (res.status === 204) {
+                    return "/images/nekaovog2.jpg"; // Ako nema slike, postavi default
+                  } else {
+                    const imageBlob = new Blob([res.data], {
+                      type: "image/jpeg",
+                    });
+                    const imageUrl = URL.createObjectURL(imageBlob);
+                    console.log("imam sliku!! " + imageUrl);
+                    return imageUrl; // Vrati URL slike
+                  }
+                })
 
-                  return "/images/nekaovog2.jpg"; // Ako nema slike, postavi default
-
-                } else {
-                  const imageBlob = new Blob([res.data], { type: "image/jpeg" });
-                  const imageUrl = URL.createObjectURL(imageBlob);
-                  console.log("imam sliku!! " + imageUrl);
-                  return imageUrl; // Vrati URL slike
-                }
-              })
-
-              .catch(() => "/images/nekaovog2.jpg") // U slučaju greške, postavi default
-
+                .catch(() => "/images/nekaovog2.jpg") // U slučaju greške, postavi default
           )
         )
           .then((logosArray) => {
@@ -79,51 +77,57 @@ function PrevActivitiesList() {
 
   const getLink = (activityId) => {
     if (role === "ROLE_ORGANIZATION") {
-        return `/organization/activity/${activityId}`;
+      return `/organization/activity/${activityId}`;
     } else if (role === "ROLE_VOLUNTEER") {
-        return `/volunteer/activity/${activityId}`;
+      return `/volunteer/activity/${activityId}`;
     } else {
-        return `/activity/${activityId}`; // Defaultni link ako nema role
+      return `/activity/${activityId}`; // Defaultni link ako nema role
     }
-};
+  };
 
-  if (loading) return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
+  if (loading)
+    return <p className="p-8 text-gray-500">Učitavam aktivnosti...</p>;
   if (error) return <p className="p-8 text-red-500">{error}</p>;
 
   return (
     <section className="p-8">
-      <h1 className="text-3xl font-bold text-gray-800 mb-8">Prethodne aktivnosti</h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-8">
+        Prethodne aktivnosti
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {activities.length === 0 ? (
           <h1>Nažalost trenutačno nema aktivnosti u ovoj kategoriji :'( </h1>
         ) : (
           activities.map((activity, index) => (
             <>
-            <Link to={getLink(activity.projectID)}>
-            <Card
-              key={index}
-              title={activity.projectname}
-              location={activity.projectlocation}
-              dates={`From: ${activity.beginningdate} To: ${activity.enddate}`}
-              organization={activity.organizationName}
-              image={pics[index]}
-            />
-            </Link>
-            
-            <div>
-              <h3>Podijeli svoje postignuće na Facebooku:</h3>
-              <FacebookShareButton
-                url={shareUrl}
-                quote={shareMessage}
-                hashtag="#Volontiranje"
-              >
-                <FacebookIcon size={32} round={true} />
-              </FacebookShareButton>
-            </div>
+              <Link to={getLink(activity.projectID)}>
+                <Card
+                  key={index}
+                  title={activity.projectname}
+                  location={
+                    "treba dodati lokaciju i hitnost u response i otkomentirati liniju ispod odnosno obriisati ovu"
+                  }
+                  // location={activity.projectlocation}
+                  startDate={activity.beginningdate}
+                  endDate={activity.enddate}
+                  organization={activity.organizationName}
+                  image={pics[index]}
+                  urgency={activity.urgent}
+                  category={activity.typeofwork}
+                />
+              </Link>
 
+              <div>
+                <h3>Podijeli svoje postignuće na Facebooku:</h3>
+                <FacebookShareButton
+                  url={shareUrl}
+                  quote={shareMessage}
+                  hashtag="#Volontiranje"
+                >
+                  <FacebookIcon size={32} round={true} />
+                </FacebookShareButton>
+              </div>
             </>
-            
-            
           ))
         )}
       </div>
--- IzvorniKod/frontend/volontiranje-app/src/pages/myActivities.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/myActivities.jsx
@@ -13,59 +13,57 @@ import PendingActivitiesList from "./assets/pendingActivitiesList";
 const BACK_URL = "http://localhost:8080";
 
 function MyActivities() {
+  console.log("na stranici sam s aktivnostima");
 
-    console.log("na stranici sam s aktivnostima");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
-    const [error, setError] = useState("");
-    const [message, setMessage] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const location = useLocation();
-    const navigate = useNavigate();
+  useEffect(() => {
+    console.log("u volonteskom home pageu");
+    const token = localStorage.getItem("token");
+    console.log("Tu je token!" + token);
+    if (!token) {
+      console.log("nema tokena!");
+      navigate("/not-authorized", { replace: true });
 
-    useEffect(() => {
-        console.log("u volonteskom home pageu");
-        const token = localStorage.getItem("token");
-        console.log("Tu je token!" + token);
-        if (!token) {
-          console.log("nema tokena!");
-          navigate("/not-authorized", { replace: true });
-    
-          return;
+      return;
+    }
+
+    console.log("saljem token na backend");
+    axios
+      .get(`${BACK_URL}/volunteer/home`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setMessage(response.data);
+        console.log("Tu sam!");
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+        console.error("Greška prilikom provjere tokena:", error.message); // Dodano ispisivanje glavne poruke
+        if (error.response) {
+          console.error("Odgovor poslužitelja:", error.response.data); // Dodano ispisivanje server response data
         }
-    
-        console.log("saljem token na backend");
-        axios
-          .get(`${BACK_URL}/volunteer/home`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          })
-          .then((response) => {
-            setMessage(response.data);
-            console.log("Tu sam!");
-          })
-          .catch((error) => {
-            console.log("error: ", error);
-            console.error("Greška prilikom provjere tokena:", error.message); // Dodano ispisivanje glavne poruke
-            if (error.response) {
-              console.error("Odgovor poslužitelja:", error.response.data); // Dodano ispisivanje server response data
-            }
-            navigate("/not-authorized", { replace: true });
-          });
-      }, [navigate]);
+        navigate("/not-authorized", { replace: true });
+      });
+  }, [navigate]);
 
-    return (
-        <div className="relative min-h-screen">
-            <div className="bg-slate-600 rounded-b-3xl text-white">
-            <NavBarLoggedInVol />
-            </div>
-            <InProgressActivitiesList/>
-            <FutureActivitiesList/>
-            <PendingActivitiesList/>
-            <PrevActivitiesList />
-        </div>
-    );
+  return (
+    <div className="relative min-h-screen">
+      <div className="bg-slate-600 rounded-b-3xl text-white">
+        <NavBarLoggedInVol />
+      </div>
+      <InProgressActivitiesList />
+      <FutureActivitiesList />
+      <PendingActivitiesList />
+      <PrevActivitiesList />
+    </div>
+  );
 }
-   
 
 export default MyActivities;
