c1866c0d80f790986aab58316261ae39a22d0491
pata <patricija.marijanovic955@gmail.com>
2025-01-18 09:47:29 +0100

    slike fix

--- IzvorniKod/frontend/volontiranje-app/src/pages/assets/inProgressActOrg.jsx
+++ IzvorniKod/frontend/volontiranje-app/src/pages/assets/inProgressActOrg.jsx
@@ -14,6 +14,7 @@ function PrevActOrg({organizationId}) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const role = localStorage.getItem("role");
+  const [pics, setPics] = useState([]);
   console.log(role);
 
   useEffect(() => {
@@ -27,6 +28,40 @@ function PrevActOrg({organizationId}) {
     )
       .then((response) => {
         console.log(response.data)
+
+        const ids = response.data.map((org) => org.projectID);
+        console.log("ids: " + ids)
+        // Pokreni zahtjeve za sve ID-eve paralelno
+        Promise.all(
+          ids.map((projectId) =>
+            axios
+              .get(`${BACK_URL}/home/project-picture/${projectId}`, {
+                responseType: "arraybuffer", // Ovisno o backendu, koristi arraybuffer za slike
+              })
+              .then((res) => {
+                if (res.status === 204) {
+
+                  return "/images/nekaovog2.jpg"; // Ako nema slike, postavi default
+
+                } else {
+                  const imageBlob = new Blob([res.data], { type: "image/jpeg" });
+                  const imageUrl = URL.createObjectURL(imageBlob);
+                  console.log("imam sliku!! " + imageUrl);
+                  return imageUrl; // Vrati URL slike
+                }
+              })
+
+              .catch(() => "/images/nekaovog2.jpg") // U slučaju greške, postavi default
+
+          )
+        )
+          .then((logosArray) => {
+            setPics(logosArray); // Postavi sve logotipe odjednom
+          })
+          .catch((err) => {
+            console.error("Error fetching logos:", err);
+          });
+
         setActivities(response.data);
         setLoading(false);
       })
@@ -66,7 +101,7 @@ function PrevActOrg({organizationId}) {
                   dates={`From: ${activity.beginningdate} To: ${activity.enddate}`}
                   //organization={activity.organizationName}
 
-                  image={"/images/nekaovog2.jpg"}
+                  image={pics[index]}
 
                 />
               </Link>
